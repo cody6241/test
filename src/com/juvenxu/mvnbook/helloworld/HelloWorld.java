ed9b17c312dda6f7914bf23751e1d2897daa2404
cody6241 <tea-88@163.com>
2016-11-04 09:49:19 +0800

    test git on 20161104

--- src/com/juvenxu/mvnbook/helloworld/HelloWorld.java
+++ src/com/juvenxu/mvnbook/helloworld/HelloWorld.java
@@ -10,5 +10,7 @@ public class HelloWorld
 	public static void main(String[] args)
 	{
 		System.out.print(new HelloWorld().sayHello());
+		System.out.println("Say hello world too, on 2016/11/4");
 	}
+	
 }
